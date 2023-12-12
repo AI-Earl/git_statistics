f67d78abc965ef585b8274ffcfaa97ea30f1781e
earl <t13503@ms.sltung.com.tw>
2023-12-12 11:28:45 +0800

    NEW: Add app.utility.logger in main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from app import app
+from app.utility.logger import logger
 from config import config
 
 if __name__ == '__main__':
+    logger.info(f'System Run in {config.MODE} Mode.')
     app.run(host="0.0.0.0", port=config.PORT)
